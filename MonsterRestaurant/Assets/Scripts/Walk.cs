b9293595215853926a83014857b65b08de68214b
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2022-08-09 23:19:45 +0900

    Floor1, 2 그래픽 적용 및 순간이동 좌표 전체 지정

--- MonsterRestaurant/Assets/Scripts/Walk.cs
+++ MonsterRestaurant/Assets/Scripts/Walk.cs
@@ -40,15 +40,58 @@ public class Walk : MonoBehaviour
             anim.SetBool("isWalking", false);
         }
 
-        if ((transform.position.x <= -9.5f) && (transform.position.x >= -11.5))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Im in Potal");
-            if (Input.GetKeyDown(KeyCode.Space))
+            //Floor1 Left
+            if ((transform.position.x <= -9.5f) && (transform.position.x >= -11.5))
             {
                 _Camera.transform.position = new Vector3(-26f, 0f, -10f);
-                _position.x = -26;
-                _position.y = -3;
+                _position.x = -28;
+                _position.y = -4;
             }
+            //Floor1 right
+            if ((transform.position.x >= 9.5f) && (transform.position.x <= 11.5))
+            {
+                _Camera.transform.position = new Vector3(26f, 0f, -10f);
+                _position.x = 17;
+                _position.y = -4;
+            }
+            //Outside Left
+            if((transform.position.x <= -36f) && (transform.position.x >= -38f))
+            {
+                _Camera.transform.position = new Vector3(-52f, 0f, -10f);
+                _position.x = -63;
+                _position.y = -4;
+            }
+            //Outside Middle
+            if ((transform.position.x >= 27f) && (transform.position.x <= 25))
+            {
+                _Camera.transform.position = new Vector3(0f, 0f, -10f);
+                _position.x = -9;
+                _position.y = -4;
+            }
+            //Floor2 right
+            if ((transform.position.x >= 36f) && (transform.position.x <= 38f))
+            {
+                _Camera.transform.position = new Vector3(52f, 0f, -10f);
+                _position.x = 43;
+                _position.y = -4;
+            }
+            //Floor2 left
+            if ((transform.position.x >= 14f) && (transform.position.x <= 16f))
+            {
+                _Camera.transform.position = new Vector3(0f, 0f, -10f);
+                _position.x = 9;
+                _position.y = -4;
+            }
+            //Floor3 left
+            if ((transform.position.x >= 40f) && (transform.position.x <= 42f))
+            {
+                _Camera.transform.position = new Vector3(26f, 0f, -10f);
+                _position.x = 35;
+                _position.y = -4;
+            }
+
         }
 
         transform.position = _position;
