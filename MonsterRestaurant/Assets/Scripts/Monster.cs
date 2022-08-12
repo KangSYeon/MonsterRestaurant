637cff191e9598b7d9faffb954f26805f5614642
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2022-08-13 00:54:35 +0900

    모든 맵 그래픽 적용

---
+++ MonsterRestaurant/Assets/Scripts/Monster.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    int number = 1;
+    string name = "Monster1";
+    string property = "µ¿È­";
+    string preferFood = "Á×À½ÀÇ ¿ÐÃ÷";
+}
